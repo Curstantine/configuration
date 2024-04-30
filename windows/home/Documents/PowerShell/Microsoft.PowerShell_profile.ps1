18e8aa449334ceaab70ee516fa1063a39a6f7674
Curstantine <curstantine@proton.me>
2024-04-30 11:20:29 +0530

    feat: add tab completion support for winget

--- windows/home/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ windows/home/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -4,6 +4,20 @@ $USERHOME = [System.Environment]::GetFolderPath("UserProfile")
 
 $ESC = [char]27
 
+# Tab Completion: handle menus
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+# Tab Completion: winget
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+        [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+        $Local:word = $wordToComplete.Replace('"', '""')
+        $Local:ast = $commandAst.ToString().Replace('"', '""')
+        winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
+
 function prompt {
     $prompt = ""
     $currentLocation = Get-Location
