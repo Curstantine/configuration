d8b25b38ea73dc6be684388cb9d63898c88929de
Curstantine <curstantine@proton.me>
2025-07-17 20:37:01 +0530

    feat: setup waybar to support multiple monitors

--- hyprland/home/.config/waybar/toggle-pipewire-profile.sh
+++
@@ -1,6 +0,0 @@
-if pactl get-default-source | grep -q "analog-stereo"; then
-	pactl set-card-profile alsa_card.pci-0000_00_1b.0 output:hdmi-stereo
-else
-	pactl set-card-profile alsa_card.pci-0000_00_1b.0 output:analog-stereo
-fi
-
