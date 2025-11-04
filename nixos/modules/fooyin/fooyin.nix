e9e431ecd3101a6c2cb1921ab59e9c38a92daf7d
Curstantine <Curstantine@proton.me>
2025-11-04 20:26:14 +0530

    chore: remove old unused envs

--- nixos/modules/fooyin/fooyin.nix
+++
@@ -1,7 +0,0 @@
-{ config, pkgs, ... }:
-{
-  home.packages = with pkgs; [ fooyin ];
-
-  xdg.configFile."fooyin/layouts/Vertical.fyl".source = ./layouts/Vertical.fyl;
-  xdg.configFile."fooyin/layouts/Zen.fyl".source = ./layouts/Zen.fyl;
-}
